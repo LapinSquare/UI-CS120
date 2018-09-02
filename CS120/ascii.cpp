66224e6432dfe47a4064f9984fded63f7de88869
Kim Huynh <citron.huynh@gmail.com>
2018-09-02 10:56:55 -0700

    Created ascii.cpp
    Beginner code.

---
+++ CS120/ascii.cpp
@@ -0,0 +1,28 @@
+/* 
+* Kim Huynh 
+* Section 07 
+* August 30 2018
+* Lab #1 - ASCII art 
+*/ 
+
+#include <iostream> 
+using namespace std; 
+
+int main(){ 
+
+//Cue ugly ASCII art. This is a cat, really.
+
+cout<<"  /\\___/\\"<<endl; 
+cout<<" |  .   . |"<<<endl; 
+cout<<" |   ___  |"<<endl;
+cout<<" __________"<<endl;
+cout<<" 0000000000"<<endl; 
+cout<<" 8080808080"<<endl; 
+cout<<" ||||||||||"<<endl; 
+cout<<" .........."<<endl; 
+cout<<" |__|  |__|"<<endl; 
+cout<<""<<endl; 
+
+return 0;
+
+}
